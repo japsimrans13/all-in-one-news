cd48c79670737ea9421c6b383307a8a4652b7790
Favour chukwuedo <fchukwuedo@gmail.com>
2022-01-28 23:36:56 +0100

    Added CSS Property for Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ export class NavBar extends Component {
                 <nav className="navbar navbar-expand-lg  bg-dark">
   <div className="container-fluid ">
     <Link className="navbar-brand text-white"  to="/">Breaking News</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler navbar-mobile-outline" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
